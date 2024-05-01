eabb0fa349f6e3f8f029e335c3c8ee503f2e6b29
Pt <165652182+ProTurqish@users.noreply.github.com>
2024-05-01 07:24:07 +0300

    Update srih.js

--- srih.js
+++ srih.js
@@ -1,25 +1,4 @@
 
-
-<script>
-        function checkCode() {
-            var code = document.getElementById('code').value;
-            var terminal = document.getElementById('terminal');
-
-            var regex = /"([^"]+)"/g;
-            var matches = code.match(regex);
-
-            if (matches && matches.length > 0) {
-                terminal.innerHTML = "Terminal Çıktısı:\n";
-                matches.forEach(function(match) {
-                    terminal.innerHTML += match.slice(1, -1) + "\n";
-                });
-            } else {
-                terminal.innerHTML = "Terminal Çıktısı:\n";
-            }
-        }
-
-    </script>
-    <script>
          function checkCode() {
             var code = document.getElementById('code').value;
             var terminal = document.getElementById('terminal');
